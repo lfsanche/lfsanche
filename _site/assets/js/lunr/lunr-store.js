b21b988d1984fff88e28cb7f51f5f84f37067b6f
Leonardo Sanchez-Aragon <39008087+lfsanche@users.noreply.github.com>
2024-10-21 21:06:46 -0500

    new theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Mi primer file!",
+        "excerpt":"1.1 Series de tiempo Estacionarias y No Estacionarias Proceso estocástico estacionario (estacionariedad estricta). El proceso estocástico \\(\\left\\{x_t: t=1,2, \\ldots\\right\\}\\) es estacionario si la distribución conjunta de \\(\\left(x_{1}, x_{2}\\right.\\), \\(\\left.\\ldots, x_{m}\\right)\\) es la mismo que la distribución conjunta de \\(\\left(x_{1+h} , x_{2+h}, \\ldots, x_{m+h}\\right)\\) para todos los números enteros \\(h \\geq...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/lfsanche/jekyll/update/2024/10/21/mi-primer-file.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Leo!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/lfsanche/my-new-url",
+        "teaser": null
+      }]
